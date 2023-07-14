6c443d607e32dcaccc20511a04e3a69a94eaad29
Eddie <emejia@serempre.com>
2023-07-14 17:59:52 -0500

    fix useTimerCountdown

--- src/test/useTimerCountdown.test.ts
+++ src/test/useTimerCountdown.test.ts
@@ -1,13 +1,20 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import useTimerCountdown from '../useTimerCountdown';
 
-jest.useFakeTimers();
-
 describe('useTimerCountdown', () => {
-  test('should start the countdown and update the time correctly', () => {
-    const { result } = renderHook(() => useTimerCountdown('0:10'));
+  jest.useFakeTimers();
 
-    expect(result.current.time).toBe('0:10');
+  it('should initialize with the provided initial time', () => {
+    const initialTime = '0:10';
+    const { result } = renderHook(() => useTimerCountdown(initialTime));
+    expect(result.current.time).toBe(initialTime);
+  });
+
+  it('should update the time every second until it reaches 0', () => {
+    const initialTime = '0:10';
+    const { result } = renderHook(() => useTimerCountdown(initialTime));
+
+    expect(result.current.time).toBe(initialTime);
 
     act(() => {
       jest.advanceTimersByTime(1000);
@@ -22,14 +29,32 @@ describe('useTimerCountdown', () => {
     expect(result.current.time).toBe('0:0');
   });
 
-  test('should call the callback when the time is up', () => {
+  it('should call the callback when the timer reaches 0', () => {
+    const initialTime = '0:5';
     const callback = jest.fn();
-    renderHook(() => useTimerCountdown('0:01', callback));
+    renderHook(() => useTimerCountdown(initialTime, callback));
 
     act(() => {
-      jest.advanceTimersByTime(60000);
+      jest.runAllTimers();
     });
 
     expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should reset the timer to the initial time', () => {
+    const initialTime = '0:10';
+    const { result } = renderHook(() => useTimerCountdown(initialTime));
+
+    act(() => {
+      jest.advanceTimersByTime(5000);
+    });
+
+    expect(result.current.time).toBe('0:5');
+
+    act(() => {
+      result.current.resetTimer();
+    });
+
+    expect(result.current.time).toBe(initialTime);
   });
 });
--- src/useTimerCountdown/useTimerCountdown.ts
+++ src/useTimerCountdown/useTimerCountdown.ts
@@ -4,28 +4,38 @@ import moment from 'moment';
 
 type TimerCallback = () => void;
 
-const useTimerCountdown = (initialTime: string, callback?: TimerCallback) => {
+function useTimerCountdown(initialTime: string, callback?: TimerCallback) {
   const [time, setTime] = useState(initialTime);
-
-  useCustomEffect(() => {
-    const timer = moment(initialTime, ['mm:ss']);
-    const interval = setInterval(() => {
-      const newTimer = timer.subtract(1, 'seconds');
-      if (newTimer.get('minutes') === 0 && newTimer.get('seconds') === 0) {
-        clearInterval(interval);
-        if (callback) {
-          callback();
+  const [timer, setTimer] = useState(moment(initialTime, ['mm:ss']));
+  let interval: NodeJS.Timer;
+  useCustomEffect(
+    () => {
+      interval = setInterval(() => {
+        const newTimer = timer.subtract(1, 'seconds');
+        if (newTimer.get('minutes') === 0 && newTimer.get('seconds') === 0) {
+          clearInterval(interval);
+          if (callback) {
+            callback();
+          }
         }
+        setTime(`${newTimer.get('minutes')}:${newTimer.get('seconds')}`);
+        setTimer(newTimer);
+      }, 1000);
+    },
+    [initialTime, callback, timer],
+    () => {
+      if (interval) {
+        clearInterval(interval);
       }
-      setTime(`${newTimer.get('minutes')}:${newTimer.get('seconds')}`);
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [initialTime, callback]);
-
-  return { time };
-};
-
+    }
+  );
+  function resetTimer() {
+    setTimer(moment(initialTime, ['mm:ss']));
+    setTime(initialTime);
+  }
+  return {
+    time,
+    resetTimer,
+  };
+}
 export default useTimerCountdown;
